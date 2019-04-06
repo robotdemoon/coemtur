47c8b515a5912fc351339bcd801a74d66d3b7646
ivanmlar <ivanmlar@outlook.com>
2019-04-06 15:56:02 -0600

    Codigo para enviar el correo

--- enviar.php
+++ enviar.php
@@ -1,3 +1,11 @@
 <?php
 
-    echo "enviar correo";
+    ini_set( 'display_errors', 1 );
+    error_reporting( E_ALL );
+    $from = "prueba@sender.com";
+    $to = "robotdemn@gmail.com";
+    $subject = "Checking PHP mail";
+    $message = "PHP mail works just fine";
+    $headers = "From:" . $from;
+    mail($to,$subject,$message, $headers);
+    echo "The email message was sent.";
