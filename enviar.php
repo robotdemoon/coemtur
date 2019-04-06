7a499740fe67d6eaf2de8027ccdf9decb1b921b9
ivanmlar <ivanmlar@outlook.com>
2019-04-06 16:07:51 -0600

    Creacion de javascript msg

--- enviar.php
+++ enviar.php
@@ -1,11 +1,23 @@
 <?php
 
-    ini_set( 'display_errors', 1 );
-    error_reporting( E_ALL );
-    $from = "prueba@sender.com";
-    $to = "robotdemn@gmail.com";
-    $subject = "Checking PHP mail";
-    $message = "PHP mail works just fine";
-    $headers = "From:" . $from;
-    mail($to,$subject,$message, $headers);
-    echo "The email message was sent.";
+    //Recibir los datos por post
+
+    if(isset($_POST['form'])){
+        $form = json_decode($_POST['form']);
+        $sender = $form['email'];
+        $msg = $form['msg'];
+        $subject = $form['subject'];
+        $name = $form['name'];
+        ini_set( 'display_errors', 1 );
+
+        //Enviamos el correo
+        error_reporting( E_ALL );
+        $from = "prueba@sender.com";
+        $to = "robotdemn@gmail.com";
+        $headers = "From:" . $sender;
+        mail($to,$subject,$msg, $headers);
+        $r = ['e' => false];
+    }else{
+        $r = ['e' => true];
+    }
+    return json_encode($r);
