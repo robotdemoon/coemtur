8be2935a8171b683e07d4a339ec91b9e6c465023
ivanmlar <ivanmlar@outlook.com>
2019-04-06 16:45:50 -0600

    test email

--- enviar.php
+++ enviar.php
@@ -8,13 +8,20 @@
         $msg = $_POST['msg'];
         $subject = $_POST['subject'];
         $name = $_POST['name'];
-        ini_set( 'display_errors', 1 );
+        /*ini_set( 'display_errors', 1 );
 
         //Enviamos el correo
         error_reporting( E_ALL );
         $to = "robotdemn@gmail.com";
         $headers = "From:" . $sender;
-        mail($to,$subject,$msg, $headers);
+        mail($to,$subject,$msg, $headers);*/
+        $from = "test@hostinger-tutorials.com";
+        $to = "robotdemn@gmail.com";
+        $subject = "Checking PHP mail";
+        $message = "PHP mail works just fine";
+        $headers = "From:" . $from;
+        mail($to,$subject,$message, $headers);
+
         $r = ["e" => false];
     }else{
         $r = ["e" => true];
